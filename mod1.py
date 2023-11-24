785100890be3118fa21db90bb156f276cbb075b9
junmooo <junmo93@hanmail.net>
2023-11-24 09:09:12 +0900

    파이썬...밀린거

---
+++ mod1.py
@@ -0,0 +1,18 @@
+def add(x,y):
+    return x+y
+    
+def sub(x,y):
+    return x-y
+if __name__ == '__main__':
+    print(add(10,3))
+    print(sub(10,3))
+    
+a=1
+b=2
+
+class Test:
+    def__init__(self):
+        self.x=0
+        self.y=0
+    def add(self):
+        return self.x + self.y
